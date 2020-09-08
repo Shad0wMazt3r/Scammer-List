6f995bf7e4005b53d2439270fa3ab64b6bcbedc2
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-09-08 09:41:01 +0530

    added main component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { SubmitComponent } from './submit/submit.component';
-import { ResultComponent } from './result/result.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SubmitComponent,
-    ResultComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/main/main.component.spec.ts
+++ client/src/app/main/main.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SubmitComponent } from './submit.component';
+import { MainComponent } from './main.component';
 
-describe('SubmitComponent', () => {
-  let component: SubmitComponent;
-  let fixture: ComponentFixture<SubmitComponent>;
+describe('MainComponent', () => {
+  let component: MainComponent;
+  let fixture: ComponentFixture<MainComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SubmitComponent ]
+      declarations: [ MainComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SubmitComponent);
+    fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
