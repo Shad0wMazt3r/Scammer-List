d9e88783ba7e43d5a0a9ea4ac1fab6789ffc3573
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-09-09 14:17:57 +0530

    added typing animation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   text = '.';
   ngOnInit() {
-  //   const target = document.getElementById('lol')
-  //   const options = {
-  //     loop: true,
-  //   }
-  //   const writer = new Typewriter(target, options)
-
-  //   writer.type(this.text).rest(12).start();
-  //   writer.removeCursor()
   }
 
 }
--- client/src/app/main/main.component.ts
+++ client/src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Typewriter from 't-writer.js';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,14 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const target = document.getElementById('loading')
+    const options = {
+      loop: true,
+      typeColor: '#40f029',
+      typeSpeed: 47,
+    }
+    const writer = new Typewriter(target, options)
+    writer.type(' Connecting to server......').rest(5000).removeCursor().start();
   }
 
 }
