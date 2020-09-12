a2a18c2f423a4c0f30a5a69b21301f3ce8f96cfd
Ujjwal Kumar <katanadeveloper01@gmail.com>
2020-09-13 00:26:06 +0530

    content policy test

--- client/src/app/services/main.service.ts
+++ client/src/app/services/main.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MainService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient
+    ) { }
 
   getResult(name, message){
-    return this.http.get(`http://54.162.146.101/?name=${name}&$message=${message}`)
+    return this.http.get(`http://54.162.146.101/?name=${name}&message=${message}`, {
+      headers: new HttpHeaders({
+        'Accept': 'text/html'
+        // 'Content-Type': 'application/json
+      })
+    })
   }
 }
